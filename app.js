0577a6ce19e3556dcf844aea5cc7052ce0290ed7
Chelovskii Maxim <foxymakc@yandex.ru>
2022-07-25 21:42:31 +0300

    pravki eslint

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/mestodb", {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.use((req, res, next) => {
   req.user = {
-    _id: "62dd39b8d9c5723408a1709c", // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '62dd39b8d9c5723408a1709c', // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
 
   next();
 });
 
-app.use("/", require("./routes/users"));
-app.use("/", require("./routes/cards"));
+app.use('/', require('./routes/users'));
+app.use('/', require('./routes/cards'));
+
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый адрес не найден' });
 });
 
 app.listen(PORT, () => {
-  console.log("App listening on port ${PORT}");
+  // eslint-disable-next-line no-template-curly-in-string, no-console
+  console.log('App listening on port ${PORT}');
 });
