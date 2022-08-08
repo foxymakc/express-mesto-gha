e9d4f747315cd66b7663fd0e4867f9b4590565ad
Chelovskii Maxim <foxymakc@yandex.ru>
2022-08-08 13:57:05 +0300

    test7id+updateuser

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -12,7 +12,7 @@ const loginValidation = celebrate({
 const createUserValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(20),
+    about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(listConditions),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
@@ -29,7 +29,7 @@ const idValidation = celebrate({
 const updateUserValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(20),
+    about: Joi.string().required().min(2).max(30),
   }),
 });
 
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const {
 } = require('../middlewares/validation');
 
 router.get('/users', getUsers);
-router.get('/users/userId', idValidation, getUserId);
+router.get('/users/:userId', idValidation, getUserId);
 router.get('/users/me', idValidation, getInfoUser);
 router.patch('/users/me', updateUserValidation, updateUser);
 router.patch('/users/me/avatar', updateAvatarValidation, updateAvatar);
